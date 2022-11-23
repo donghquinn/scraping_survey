d61fbd3559db8eea53496ce7200a5d181f868565
Dong H. Kim <ehdgus1524@gmail.com>
2022-11-23 10:45:30 +0900

    [CHANGEd] desciprtion

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,9 @@ function Main() {
   return (
     <div>
       <h1 className='quest-title'> 246조 설문조사</h1>
+      <p className='description'>소비자 행동 분석 팀 프로젝트 과제 수행을 위한 설문조사입니다.</p>
+      <p className='description'>부디 모든 질문에 응답하시고 제일 아래의 '제출 버튼'을 눌러주시기 바랍니다.</p>
+      <p className='description'>직접 입력하는 응답의 경우, 응답 입력 후 '확인' 버튼을 꼭 눌러주시기 바랍니다.</p>
       <div className='divider'></div>
       <Questions0></Questions0>
       <div className='divider'></div>
