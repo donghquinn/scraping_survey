7b7ef6dd3e9860d4db1683d85cb34d14eeee683a
Dong H. Kim <ehdgus1524@gmail.com>
2022-11-23 11:02:25 +0900

    [CHANGED] submit

--- components/submit.tsx
+++ components/submit.tsx
@@ -1,4 +1,4 @@
-import axios from "axios"
+import { AxiosError } from "axios";
 import Link from "next/link";
 import { useRecoilState } from "recoil";
 import { requestDataInput, totalCountResponse } from "./test";
@@ -7,20 +7,19 @@ function Submit(props: {age: string, platforms: string, reasons: string}) {
   const {age, platforms, reasons} = props;
   const [totalCount, setTotalCount] = useRecoilState(totalCountResponse)
 
-  const submitButton = async(age: string, platforms: string, reasons: string) => {
-    const response = await requestDataInput(age, platforms, reasons);
-    
-    const {returnObject, surveytotal, platformReturnData, reasonsWithPlatforms} = response.dataRes;
-    setTotalCount(surveytotal);
-
-    return {returnObject, surveytotal, platformReturnData, reasonsWithPlatforms}
-  }
-
   return (
     <div>
-      <Link href='/result'>
-        <button className="btn" onClick={async() => await submitButton(age, platforms, reasons)}>제출하기</button>
-      </Link>
+        <button className="btn" onClick={async() => {
+           try {
+            const response = await requestDataInput(age, platforms, reasons);
+            
+            return (
+              <div>완료</div>
+            )
+           } catch (error) {
+            throw new AxiosError('[ERROR!]');
+           }
+        }}>제출하기</button>
     </div>
   )
 }
